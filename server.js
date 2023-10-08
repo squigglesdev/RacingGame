cee48d78b149fe99f1814177bbcd25bfb4d4a98c
Squiggles <luke.saynor@gmail.com>
2023-10-08 20:13:48 +0100

    update server for new networking stack

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ wss.on('connection', (ws) => {
 
         if (data.type === 'newPlayer') {
             // Handle new player connection
-            clients[data.name] = ws;
+            clients[data.id] = ws;
             
             // Send existing player data to the new player
             for (let playerName in clients) {
-                if (playerName !== data.name) {
+                if (playerName !== data.id) {
                     // Send the existing player's data to the new player
-                    ws.send(JSON.stringify({ type: 'newPlayer', name: playerName }));
+                    ws.send(JSON.stringify({ type: 'newPlayer', id: playerName }));
                 }
             }
 
@@ -27,42 +27,42 @@ wss.on('connection', (ws) => {
         } else if (data.type === 'disconnect') {
             // Handle player disconnection
             broadcastPlayerDisconnect(data);
-            delete clients[data.name];
+            delete clients[data.id];
         }
     });
 });
 
 // console log when server is running
 wss.on('listening', () => {
-    console.log('Server listening on port 8080');
+    console.log('Server listening on port 8080 :)');
 });
 
 function broadcastNewPlayer(data) {
     // Broadcast new player information to all clients except the sender
-    console.log("New player: " + data.name);
+    console.log("New player: " + data.id);
     wss.clients.forEach((client) => {
-        if (client !== clients[data.name] && client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify({ type: 'newPlayer', name: data.name, pos: data.pos, angle: data.angle }));
+        if (client !== clients[data.id] && client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({ type: 'newPlayer', name: data.name, pos: data.pos, angle: data.angle, id: data.id }));
         }
     });
 }
 
 function broadcastUpdatePlayer(data) {
     // Broadcast player position and angle updates to all clients except the sender
-    console.log("Update player: " + data.name);
+    console.log("Update player: " + data.id);
     wss.clients.forEach((client) => {
-        if (client !== clients[data.name] && client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify({ type: 'updatePlayer', name: data.name, pos: data.pos, angle: data.angle }));
+        if (client !== clients[data.id] && client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({ type: 'updatePlayer', name: data.name, pos: data.pos, angle: data.angle, id: data.id }));
         }
     });
 }
 
 function broadcastPlayerDisconnect(data) {
     // Broadcast player disconnection to all clients except the sender
-    console.log("Disconnect player: " + data.name);
+    console.log("Disconnect player: " + data.id);
     wss.clients.forEach((client) => {
-        if (client !== clients[data.name] && client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify({ type: 'playerDisconnect', name: data.name }));
+        if (client !== clients[data.id] && client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({ type: 'playerDisconnect', id: data.id }));
         }
     });
 }
