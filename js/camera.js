bf3af9771436037008296706ac8d0ceabf546bf1
Squiggles <80328580+squigglesdev@users.noreply.github.com>
2023-10-09 15:42:44 +0100

    add zoom camera property

--- js/camera.js
+++ js/camera.js
@@ -1,11 +1,17 @@
 class Camera {
-    constructor(lerpAmount) {
+    constructor(lerpAmount, zoom) {
         this.pos = createVector(0, 0);
         this.lerpAmount = lerpAmount;
+        this.zoom = zoom;
     }
   
     follow(target) {
         this.pos.x = lerp(this.pos.x, target.pos.x, this.lerpAmount);
         this.pos.y = lerp(this.pos.y, target.pos.y, this.lerpAmount);
+    }
+
+    zoomTo(value) {
+        this.zoom += 0.5;
+        this = max( 0, min(this.zoom, value) )
     }
 }
